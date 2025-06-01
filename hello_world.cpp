06b6fda044895230c42815dd87fe87a355c0cb34
Enafsky <enafutev@gmail.com>
2025-06-01 20:32:53 +0300

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,7 +2,7 @@
 
 int main() {
     std::string name;
-    std::cout << "Enter your name: ";  // просим ввести имя
+    std::cout << "Enter your name: "; 
     std::cin >> name;
     std::cout << "Hello, world from " << name << "!";  // выводим приветствие
     return 0;
